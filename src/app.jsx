78733d45ea316f5d15c1101a46c3b4de0c842905
MatheusZamo <19matheushenrique03@gmail.com>
2025-09-22 09:11:00 -0300

    Feat: game creation (no React Query yet)

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,13 @@
 import { useQuery } from "@tanstack/react-query"
 
 const fetchGames = () =>
-  fetch("http://localhost:3000/games")
-    .then((res) => res.json())
-    .then((data) => data)
+  fetch("http://localhost:3000/games").then((res) => res.json())
 
 const Games = () => {
   const { isError, isLoading, isSuccess, error, data } = useQuery({
     queryKey: ["games"],
     queryFn: fetchGames,
-    refetchOnWindowFocus: false,
+    retry: false,
     staleTime: Infinity,
   })
 
@@ -36,17 +34,30 @@ const Games = () => {
 }
 
 const Form = () => {
+  const addGame = (e) => {
+    e.preventDefault()
+    const { title, url } = e.target.elements
+    fetch("http://localhost:3000/games", {
+      method: "POST",
+      body: JSON.stringify({
+        id: crypto.randomUUID(),
+        title: title.value,
+        url: url.value,
+      }),
+      headers: { "Content-type": "application/json" },
+    })
+  }
   return (
     <div>
       <h3>Adicionar Jogo</h3>
-      <form>
+      <form onSubmit={addGame}>
         <label>
           Nome:
-          <input type="text" autoFocus />
+          <input name="title" type="text" autoFocus />
         </label>
         <label>
           Link:
-          <input type="url" />
+          <input name="url" type="url" />
         </label>
         <button>Adicionar</button>
       </form>
